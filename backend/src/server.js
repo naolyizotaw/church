29289abd44d55d008e7fdc10ff059fac84c8c6b4
naolyizotaw <naolyizotaw@gmail.com>
2025-12-14 11:18:38 +0300

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 
 
 
 const app = express();
+const __dirname = path.resolve();
 
 app.get("/api/auth", (req, res) => {
     res.send("Hello World");
@@ -18,8 +20,17 @@ app.get("/api/auth/register", (req, res) => {
 
 
 
+if (process.env.NODE_ENV !== "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-const PORT = process.env.PORT || 6002;
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    });
+}
+
+
+
+const PORT = process.env.PORT || 6001;
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`)
 });
